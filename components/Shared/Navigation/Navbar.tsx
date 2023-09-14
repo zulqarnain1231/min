79d5d04915e4235e979156d4d92c56117f8055fa
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-13 21:14:16 -0700

    made the navbar responsive

--- components/Shared/Navigation/Navbar.tsx
+++ components/Shared/Navigation/Navbar.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-
+import * as Icons from "../../../constants/Svg/Icons";
+import { BiMenuAltRight } from "react-icons/bi";
 const Navbar = () => {
   const menu = [
     {
@@ -23,7 +24,10 @@ const Navbar = () => {
   ];
   return (
     <nav className="w-full h-[70px] bg-transparent flex items-center justify-between md:px-6 px-2 pt-4">
-      <Link href={"/"} className="h-[60px] w-[250px] relative">
+      <Link
+        href={"/"}
+        className="md:h-[60px] h-[46px] md:w-[250px] w-[200px] relative"
+      >
         <Image
           className="w-full h-full object-contain"
           src={"/Assets/Logo.png"}
@@ -31,7 +35,7 @@ const Navbar = () => {
           fill
         />
       </Link>
-      <div className="h-full flex items-center justify-start gap-4">
+      <div className="h-full lg:flex hidden items-center justify-start gap-8">
         <div className="h-full flex justify-start items-center gap-4">
           {menu.map((item: any, index: number) => (
             <Link
@@ -43,13 +47,14 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-        <button className="h-[54px] w-[230px] flex items-center justify-center gap-2 text-white-main text-base font-normal">
-          <span className="">
-            <Image />
-          </span>
+        <button className="h-[54px] w-[230px] flex items-center justify-center gap-2 bg-brand-main rounded-[100px] text-white-main text-base font-normal">
+          <Icons.LineIcon ClassName="text-[24px]" fill="" stroke="" />
           LINEでデモ作成
         </button>
       </div>
+      <button className="flex lg:hidden items-center justify-center bg-transparent">
+        <BiMenuAltRight className="text-[28px] text-black-main" />
+      </button>
     </nav>
   );
 };
