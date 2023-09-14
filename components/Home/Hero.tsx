eaba980dccb647a5d4c2fcd31f6d9998676cd87e
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-14 01:34:16 -0700

    added the drawer in navbar

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ComponentWrapper from "../Shared/Wrappers/ComponentWrapper";
 import Image from "next/image";
 
+import * as Icons from "../../constants/Svg/Icons";
 const Hero = () => {
   return (
     <ComponentWrapper id="hero" style="w-full h-full">
       <div className="w-full h-full flex flex-col items-center justify-start gap-4">
-        <div className="w-full h-full grid lg:grid-cols-2 lg:gap-0 gap-8 place-items-center">
+        <div className="w-full lg:h-[80%] grid lg:grid-cols-2 lg:gap-0 gap-8 place-items-center">
           <div className="w-full flex flex-col items-center justify-start gap-8 pt-5 lg:pt-0">
-            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[72px] sm:h-[60px] h-[50px] relative">
+            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[72px] sm:h-[60px] h-[50px] relative pointer-events-none">
               <Image
                 className="h-full w-full object-contain"
                 src={"/Assets/Hero/title.png"}
@@ -16,7 +17,7 @@ const Hero = () => {
                 fill
               />
             </div>
-            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[96px] sm:h-[80px] h-[60px] relative">
+            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[96px] sm:h-[80px] h-[60px] relative pointer-events-none">
               <Image
                 className="h-full w-full object-contain"
                 src={"/Assets/Hero/content-yellow.png"}
@@ -24,7 +25,7 @@ const Hero = () => {
                 fill
               />
             </div>
-            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[68px] sm:h-[50px] h-[40px] relative">
+            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[68px] sm:h-[50px] h-[40px] relative pointer-events-none">
               <Image
                 className="h-full w-full object-contain"
                 src={"/Assets/Hero/rounded-content.png"}
@@ -32,7 +33,7 @@ const Hero = () => {
                 fill
               />
             </div>
-            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[100px sm:h-[80px] h-[60px] relative">
+            <div className="lg:w-[470px] sm:w-[350px] w-[300px] lg:h-[100px sm:h-[80px] h-[60px] relative pointer-events-none">
               <Image
                 className="h-full w-full object-contain"
                 src={"/Assets/Hero/price.png"}
@@ -41,15 +42,19 @@ const Hero = () => {
               />
             </div>
           </div>
-          <div className="w-full  sm:h-[400px] h-[300px] flex-shrink-0 relative">
+          <div className="w-full  sm:h-[400px] h-[250px] flex-shrink-0 relative pointer-events-none">
             <Image
-              className="w-full h-full md:object-fill object-contain"
+              className="w-full h-full object-fill"
               src={"/Assets/Hero/Hero.png"}
               alt=""
               fill
             />
           </div>
         </div>
+        <button className="h-[70px] sm:w-[420px] w-full flex items-center justify-center gap-2 bg-brand-main rounded-[100px] text-white-main text-lg font-normal lg:-mt-8">
+          <Icons.LineIcon ClassName="text-[28px]" fill="" stroke="" />
+          LINEで今すぐデモ作成
+        </button>
       </div>
     </ComponentWrapper>
   );
--- components/Shared/Navigation/Navbar.tsx
+++ components/Shared/Navigation/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import * as Icons from "../../../constants/Svg/Icons";
 import { BiMenuAltRight } from "react-icons/bi";
+import { ImCross } from "react-icons/im";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 const Navbar = () => {
   const menu = [
     {
@@ -22,40 +26,80 @@ const Navbar = () => {
       route: "#",
     },
   ];
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevvalue) => !prevvalue);
+  };
   return (
-    <nav className="w-full h-[70px] bg-transparent flex items-center justify-between md:px-6 px-2 pt-4">
-      <Link
-        href={"/"}
-        className="md:h-[60px] h-[46px] md:w-[250px] w-[200px] relative"
+    <>
+      <nav className="w-full h-[70px] bg-transparent flex items-center justify-between md:px-6 px-2 pt-4">
+        <Link
+          href={"/"}
+          className="md:h-[60px] h-[46px] md:w-[250px] w-[200px] relative"
+        >
+          <Image
+            className="w-full h-full object-contain"
+            src={"/Assets/Logo.png"}
+            alt=""
+            fill
+          />
+        </Link>
+        <div className="h-full lg:flex hidden items-center justify-start gap-8">
+          <div className="h-full flex justify-start items-center gap-4">
+            {menu.map((item: any, index: number) => (
+              <Link
+                href={item.route}
+                key={index}
+                className="text-black-main text-base font-normal"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <button className="h-[54px] w-[230px] flex items-center justify-center gap-2 bg-brand-main rounded-[100px] text-white-main text-base font-normal">
+            <Icons.LineIcon ClassName="text-[24px]" fill="" stroke="" />
+            LINEでデモ作成
+          </button>
+        </div>
+        <button
+          onClick={toggleDrawer}
+          className="flex lg:hidden items-center justify-center bg-transparent"
+        >
+          <BiMenuAltRight className="text-[28px] text-black-main" />
+        </button>
+      </nav>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="right"
+        style={{ width: "100%" }}
+        className="bg-white-main/30"
       >
-        <Image
-          className="w-full h-full object-contain"
-          src={"/Assets/Logo.png"}
-          alt=""
-          fill
-        />
-      </Link>
-      <div className="h-full lg:flex hidden items-center justify-start gap-8">
-        <div className="h-full flex justify-start items-center gap-4">
+        <div className="w-full flex flex-col items-start justify-start">
+          <div className="w-full flex items-center justify-end p-4 border-b-[2px]">
+            <button onClick={toggleDrawer}>
+              <ImCross className="text-[24px] text-black-main" />
+            </button>
+          </div>
           {menu.map((item: any, index: number) => (
             <Link
               href={item.route}
               key={index}
-              className="text-black-main text-base font-normal"
+              className={`text-black-main w-full text-base font-normal p-4 border-b-[2px]
+              `}
             >
               {item.name}
             </Link>
           ))}
+          <div className="w-full flex items-center justify-center mt-8">
+            <button className="h-[54px] w-[230px] flex items-center justify-center gap-2 bg-brand-main rounded-[100px] text-white-main text-base font-normal">
+              <Icons.LineIcon ClassName="text-[24px]" fill="" stroke="" />
+              LINEでデモ作成
+            </button>
+          </div>
         </div>
-        <button className="h-[54px] w-[230px] flex items-center justify-center gap-2 bg-brand-main rounded-[100px] text-white-main text-base font-normal">
-          <Icons.LineIcon ClassName="text-[24px]" fill="" stroke="" />
-          LINEでデモ作成
-        </button>
-      </div>
-      <button className="flex lg:hidden items-center justify-center bg-transparent">
-        <BiMenuAltRight className="text-[28px] text-black-main" />
-      </button>
-    </nav>
+      </Drawer>
+    </>
   );
 };
 
